c1821cc7efaa5bff47e70d10488e86d1d3ebfbb8
Majid Syed <msyed@enova.com>
2018-10-19 22:52:02 -0500

    added redux dev tools

--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
 import React from 'react'
 import { render } from 'react-dom'
 import { createStore } from 'redux'
+import { devToolsEnhancer } from 'redux-devtools-extension';
 import { Provider } from 'react-redux'
 import App from './components/App'
 import rootReducer from './reducers'
 
-const store = createStore(rootReducer)
+const store = createStore(rootReducer, /* preloadedState, */ devToolsEnhancer(
+  // Specify name here, actionsBlacklist, actionsCreators and other options if needed
+));
 
 render(
   <Provider store={store}>
